b0d700d2cb61dff3bbcf922b645a8c166fdc66b5
Koremochi <84866292+Koremochi@users.noreply.github.com>
2024-03-22 16:37:53 +0100

    Create text_node_to_html_node(text_node) method
    Make it return a LeafNode compared to the text_type

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
 from textnode import TextNode
-from htmlnode import HTMLNode
+from htmlnode import *
+
+def text_node_to_html_node(text_node):
+    text_type_list = ["text", "bold", "italic", "code", "link", "image"]
+    leaf_node = None
+    if not text_node.text_type in text_type_list:
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
+    elif text_node.text_type == "text":
+        leaf_node = LeafNode(None, text_node.text, None)
+    elif text_node.text_type == "bold":
+        leaf_node = LeafNode("b", text_node.text, None)
+    elif text_node.text_type == "italic":
+        leaf_node = LeafNode("i", text_node.text, None)
+    elif text_node.text_type == "code":
+        leaf_node = LeafNode("code", text_node.text, None)
+    elif text_node.text_type == "link":
+        leaf_node = LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == "image":
+        leaf_node = LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+
+    return leaf_node
 
 def main():
     node = TextNode("This is a text node", "bold", "https://www.boot.dev")
